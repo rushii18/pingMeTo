6edad75089f37bd5e7eef890b65fd5db6d237509
Rushi <rushigulave345@gmail.com>
2024-06-25 22:21:12 +0530

    update

--- src/All/componants/api.js
+++ src/All/componants/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const API_BASE_URL = "http://localhost:5151";
+// export const API_BASE_URL = "http://localhost:5151";
+export const API_BASE_URL = "https://8b14-115-96-216-99.ngrok-free.app";
 
 const token = localStorage.getItem("token");
 
